85da5c03ea84fe384bd1c6a5c968895888f5f4d2
Shiyang Chen <csycfl@gmail.com>
2024-07-18 02:47:01 +0000

    add chunk prefill test

--- splitwise/benchmark/start_infer.sh
+++ splitwise/benchmark/start_infer.sh
@@ -1,2 +1,3 @@
 export RAY_NUM_CPUS=64
-python /data/vllm/vllm/entrypoints/api_server.py --model /data/mistral-7b-instruct-v0_2 -tp 2 --enforce-eager --disable-custom-all-reduce
+# export VLLM_ATTENTION_BACKEND=XFORMERS
+python /data/vllm/vllm/entrypoints/api_server.py --model /data/mistral-7b-instruct-v0_2 -tp 2 --enforce-eager --disable-custom-all-reduce --enable-chunked-prefill
