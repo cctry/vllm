e32cce226c17b74549800ac5a837f32c99e9c380
Shiyang Chen <csycfl@gmail.com>
2024-07-31 05:38:11 +0000

    refactor worker

--- splitwise/worker.py
+++ splitwise/worker.py
@@ -1,97 +1,108 @@
 import time
 from itertools import accumulate
 from typing import Dict, List, Optional, Tuple
+import rdma_transport
 
-import block_copy
 import torch
 import torch.multiprocessing as mp
 import utils
-from kv_comm import KVComm
+
+# from kv_comm import KVComm
 import random
 
 from vllm.worker.worker import Worker
+
 
 KV_TIMEOUT = 30
 BUFFER_SIZE = 1 * 1024 * 1024  # 4MB
 
 
+class KVComm:
+    def __init__(
+        self,
+        cache: List[torch.Tensor],
+        local_addr: str,
+        role: str,
+    ):
+        self.cache = cache
+        self.rank = cache[0].device.index
+        self.device_id = utils.get_real_device_id(self.rank)
+        self.local_addr = local_addr
+        self.num_layer = len(cache)
+        self.shape = tuple(cache[0].shape)
+        self.block_size = (
+            self.shape[-1]
+            * self.shape[-2]
+            * self.shape[-3]
+            * cache[0].dtype.itemsize
+        )  # in bytes
+        self.local_base = [t.data_ptr() for t in cache]
+        self.kv_stride = cache[0].stride[0]
+        # TODO: start RDAM thread
+        if role == "server":
+            self.server = rdma_transport.RdmaServer(
+                self.local_addr, self.device_id
+            )
+        elif role == "client":
+            self.clients = {}
+            self.remote_base_ptr = {}
+
+    def push_kv(self, request_id, kv_addr, layers, block_ids):
+        info = next(info for info in kv_addr if info["device"] == self.rank)
+        server_addr = f'{info["host"]}:{info["port"]}'
+        if server_addr not in self.clients:
+            client = rdma_transport.RdmaClient(self.local_addr, self.device_id)
+            remote_base_ptr = client.connect(server_addr)  # Can we do this?
+            self.clients[server_addr] = client
+            self.remote_base_ptr[server_addr] = remote_base_ptr
+        else:
+            client = self.clients[server_addr]
+            remote_base_ptr = self.remote_base_ptr[server_addr]
+
+        for blk in block_ids:
+            for layer in layers:
+                local_base = self.local_base[layer]
+                remote_base = remote_base_ptr[layer]
+                client.send(
+                    local_base,
+                    remote_base,
+                    blk * self.block_size,
+                    # metadata?
+                )
+                client.send(
+                    local_base + self.kv_stride,
+                    remote_base + self.kv_stride,
+                    blk * self.block_size,
+                    # metadata?
+                )
+        
+        def wait_kv(self, request_id):
+            pass # TODO
+
+
 class WorkerSplitwise(Worker):
     def setup(self):
-        mp.set_start_method("spawn")
+        # Assume all tensors are the same
         cache = self.gpu_cache[0]
         assert self.local_rank == cache.device.index
-        block_shape = tuple(cache.shape)[-3:]
-        self._manager = mp.Manager()
-        self.flags = self._manager.dict()
-        self.requests_queue = mp.Queue()
-        host = utils.set_NIC(self.local_rank, False)
+        info = utils.detect_NIC(self.local_rank)
+        addr = info["address"]
         self.kv_addr = {}
-        # compile the gather/scatter kernels
-        if self.is_driver_worker:
-            block_size = (
-                cache.shape[-1]
-                * cache.shape[-2]
-                * cache.shape[-3]
-                * cache.dtype.itemsize
-            )
-            num_packing_blocks = BUFFER_SIZE // block_size
-            assert BUFFER_SIZE % block_size == 0
-            block_copy.get_block_copy(num_packing_blocks, block_size)
-        return block_shape, cache.dtype, cache.device, host
-
-    def get_kv_blocks_data(
-        self,
-        block_ids: List[int],
-        layers: Optional[List[int]] = None,
-    ):
-        """This function returns a list of block metadata for CUDA IPC.
-        The blocks are ordered as tokens -> layers -> (k, v).
-        For example, the layout of blocks is (S/16, L, 2 : 2L, 2, 1)
-        """
-        blocks = []
-        if layers is None:
-            layers = range(len(self.gpu_cache))
-        for block_id in block_ids:
-            for layer in (self.gpu_cache[i] for i in layers):
-                blocks.append(layer[0, block_id, ...])
-                blocks.append(layer[1, block_id, ...])
-        blocks = [utils.wrap_tensor(block) for block in blocks]
-        tensor_data = [mp.reductions.reduce_tensor(block) for block in blocks]
-        return tensor_data
+        return addr
 
     def decode_kv_init(self, port: int):
         """Initialize the KV cache communicator as the decode worker"""
-        shape, dtype, device, host = self.setup()
+        host = self.setup()
         self.port = port + self.local_rank
         addr = f"{host}:{self.port}"
-        self.kv_comm = KVComm(
-            device,
-            dtype,
-            shape,
-            "server",
-            self.requests_queue,
-            self.flags,
-            BUFFER_SIZE,
-            local_addr=addr,
-        )
-        self.kv_comm.start()
+        self.kv_comm = KVComm(self.gpu_cache, addr, "server")
         return {"device": self.local_rank, "host": host, "port": self.port}
 
     def prefill_kv_init(self, layer_wise=-1):
         """Initialize the KV cache communicator as the prefill worker"""
-        shape, dtype, device, host = self.setup()
+        host = self.setup()
         port = random.randint(40000, 60000)
-        self.kv_comm = KVComm(
-            device,
-            dtype,
-            shape,
-            "client",
-            self.requests_queue,
-            self.flags,
-            BUFFER_SIZE,
-            local_addr=f"{host}:{port}"
-        )
-        self.kv_comm.start()
+        self.kv_comm = KVComm(self.gpu_cache, f"{host}:{port}", "client")
 
         layers = self.model_runner.model.model.layers
         block_size = self.model_runner.block_size
@@ -120,7 +131,9 @@ class WorkerSplitwise(Worker):
                 for req_id, block_id in zip(args[3].request_ids, block_ids):
                     addr = self.kv_addr[req_id]
                     block_id = torch.unique_consecutive(block_id)
-                    self.push_kv(req_id, block_id.tolist(), layers, addr)
+                    self.kv_comm.push_kv(
+                        req_id, block_id.tolist(), layers, addr
+                    )
                 return output
 
             return new_forward if is_push else old_forward
@@ -130,49 +143,12 @@ class WorkerSplitwise(Worker):
 
         return {"device": self.local_rank, "host": host}
 
-    def pull_kv(self, request_id: str, block_ids: List[int]):
-        """Listen for the kv cache."""
-        tensor_data = self.get_kv_blocks_data(block_ids)
-        self.flags[request_id] = False
-        self.requests_queue.put(
-            (
-                request_id,
-                tensor_data,
-            )
-        )
-        start = time.time()
-        while not self.flags[request_id]:
-            if time.time() - start > KV_TIMEOUT:
-                raise TimeoutError(f"KV cache pull for {request_id} timeout")
-            time.sleep(0.1)
-        self.flags.pop(request_id)
-
-    def push_kv(
-        self, request_id: str, block_ids: List[int], layers: List[int], kv_addr
-    ):
-        """Push the kv cache to the decode worker"""
-        info = next(
-            info for info in kv_addr if info["device"] == self.local_rank
-        )
-        tensor_data = self.get_kv_blocks_data(block_ids, layers)
-        self.requests_queue.put(
-            (request_id, info["host"], info["port"], tensor_data)
-        )
-
     def finish_push_kv(self, request_id: str):
         """Wait for the push_kv to finish.
-        This function is non-blocking.
         """
-        self.flags[request_id] = False
-        self.requests_queue.put((request_id, None, None, None))
-        start = time.time()
-        while not self.flags[request_id]:
-            if time.time() - start > KV_TIMEOUT:
-                raise TimeoutError(f"KV cache push for {request_id} timeout")
-            time.sleep(0.1)
-        self.flags.pop(request_id)
+        self.kv_comm.wait_kv(request_id) # block here
         self.kv_addr.pop(request_id)
 
-    def add_kv_addr(self, request_id, addr):
+    def add_kv_addr(self, request_id, addr): # Will be called from driver
         assert request_id not in self.kv_addr
         self.kv_addr[request_id] = addr
