30345f6033c668f6fe56830b08d144c814898552
Shiyang Chen <csycfl@gmil.com>
2024-08-13 02:50:23 +0000

    change benchmark

--- splitwise/benchmark/feed_data.sh
+++ splitwise/benchmark/feed_data.sh
@@ -1,43 +1,40 @@
 export RAY_NUM_CPUS=64
-num_req=100
+num_req=50
 tp=8
 
 model_path=/data
-model_name=Meta-Llama-3-70B
+model_name=Meta-Llama-3-70B-dummy
+
+port=8000
 
 declare -a inputs=(
-  "1024 50"
-  "2048 100"
-  "4096 150"
-  "8192 200"
-  "16384 512"
+#   "1024 50"
+#   "2048 100"
+#   "4096 150"
+#   "8192 200"
+#   "16384 512"
   "32768 512"
 #   "65536 512"
-#   "365536 512"
-# "65536 512"
-# "32768 512"
-# "32768 2"
-# "1024 1024"
 )
 
 
-for qps in 1.25 1.5 1.75 2
+for qps in 1 # 0.25 0.5 0.75 1 1.5 2
 do
     for input in "${inputs[@]}"
     do
         prompt=$(echo $input | cut -d' ' -f1)
         resp=$(echo $input | cut -d' ' -f2)
-        out_file="${model_name}_$tp"
+        out_file="${model_name}_${tp}_pull"
         
         message="${tp}_prompt_${prompt}_${resp}_${qps}"
         python test.py \
             --model "$model_path/$model_name" \
-            --text-file pg34283.txt \
+            --text-file pg57126.txt \
             --response-length $resp \
             --prompt-length $prompt \
             --num-request $num_req \
             --qps $qps \
-            --port 8000 \
+            --port $port \
             --message $message
 
        
@@ -53,6 +50,7 @@ do
             echo "Error at prompt $prompt resp $resp qps $qps."
             break
         fi
-        sleep 1
+        echo "$prompt resp $resp qps $qps finished"
+        sleep 3
     done
 done
--- splitwise/benchmark/start_infer.sh
+++ splitwise/benchmark/start_infer.sh
@@ -2,10 +2,12 @@ export RAY_NUM_CPUS=64
 export VLLM_ENGINE_ITERATION_TIMEOUT_S=3600
 # export VLLM_ATTENTION_BACKEND=XFORMERS
 model_path=/data
-model_name=Meta-Llama-3-70B
+# model_name=Meta-Llama-3-70B
+# model_name=mistral-7b-instruct-v0_2
+model_name=Meta-Llama-3-70B-dummy
 TP=8
 # python api_server.py --model $model_path/$model_name  -tp $TP --enforce-eager --disable-custom-all-reduce #--enable-chunked-prefill 
 
 python api_server.py --model "$model_path/$model_name" \
-    --enforce-eager --disable-custom-all-reduce \
+    --enforce-eager --disable-custom-all-reduce --load-format dummy \
     -tp "$TP" > "${model_name}_$TP.txt"
