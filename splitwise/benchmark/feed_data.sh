564adb76dc356b26872f979b1d4a0dd18801a998
Shiyang Chen <csycfl@gmail.com>
2024-08-16 23:07:46 +0000

    add multi-decode worker

--- splitwise/benchmark/feed_data.sh
+++ splitwise/benchmark/feed_data.sh
@@ -6,6 +6,8 @@ model_path=/data
 model_name=Meta-Llama-3-70B-dummy
 
 port=8000
+host="localhost,10.117.208.39"
+# host="localhost"
 
 declare -a inputs=(
 #   "8192 128"
@@ -13,12 +15,12 @@ declare -a inputs=(
 #   "32768 128"
 #   "8192 512"
 #   "16384 512"
-#   "32768 512"
-  "64000 1"
+  "32768 512"
+#   "64000 1"
 )
 
 
-for qps in 1 #1 2 3 4 # 0.25 0.5 0.75 1 1.5 2
+for qps in 1 #2 3 4 # 0.25 0.5 0.75 1 1.5 2
 do
     for input in "${inputs[@]}"
     do
@@ -35,15 +37,16 @@ do
             --num-request $num_req \
             --qps $qps \
             --port $port \
+            --host $host \
             --message $message
 
        
-        grep "^\[test${message}\]" ${out_file}.txt > ${out_file}_prompt${prompt}_resp${resp}_qps${qps}_request.txt
-        grep '^INFO .* metrics.py:341' ${out_file}.txt > ${out_file}_prompt${prompt}_resp${resp}_qps${qps}_token.txt
+        # grep "^\[test${message}\]" ${out_file}.txt > ${out_file}_prompt${prompt}_resp${resp}_qps${qps}_request.txt
 
         start_line=$(grep -n "^\[test${message}\]" ${out_file}.txt | head -1 | cut -d: -f1)
         sed -n "${start_line},\$p" ${out_file}.txt > temp_extracted.txt
         grep '^INFO .* async_llm_engine.py:247' temp_extracted.txt > ${out_file}_prompt${prompt}_resp${resp}_qps${qps}_iteration.txt
+        grep '^INFO .* metrics.py:341' ${out_file}.txt > ${out_file}_prompt${prompt}_resp${resp}_qps${qps}_token.txt
         rm temp_extracted.txt
 
         if [ $? -ne 0 ]; then
